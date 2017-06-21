db1ae9060dc0411345d1ebe8bdc4731ff1a33ebf
Andrea Savio <andrea@yousician.com>
2017-06-21 11:39:14 +0300

    Rename yousiciandsp module to yousiciannative

--- modules/juce_audio_devices/native/juce_android_Midi.cpp
+++ modules/juce_audio_devices/native/juce_android_Midi.cpp
@@ -149,7 +149,7 @@ JUCE_JNI_CALLBACK (JUCE_JOIN_MACRO (JUCE_ANDROID_ACTIVITY_CLASSNAME, _00024JuceM
     reinterpret_cast<AndroidMidiInput*> (host)->receive (byteArray, offset, count, timestamp);
 }
 
-JUCE_JNI_CALLBACK(com_yousician_yousiciandsp_MidiUsbToJuce_00024InputPort, handleReceive,
+JUCE_JNI_CALLBACK(com_yousician_yousiciannative_MidiUsbToJuce_00024InputPort, handleReceive,
     void, (JNIEnv* env, jobject device, jlong host, jbyteArray byteArray,
     jint offset, jint count, jlong timestamp))
 {
